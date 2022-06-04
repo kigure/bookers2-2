d57d31cd39090b5eb9254becde941f4a659e5a41
EC2 Default User <ec2-user@ip-172-31-92-60.ec2.internal>
2022-06-04 17:28:04 +0000

    [follow機能追加完了]

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,18 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :bookcomments, only: [:create, :destroy], as: 'book_comments'
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
   
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
