9c3451f05d4fe19f15078bd59f1e99d355a71e49
EC2 Default User <ec2-user@ip-172-31-92-60.ec2.internal>
2022-06-06 18:35:21 +0000

    [検索機能追加]

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   
+  get "searche" => "searches#searche"
   
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
